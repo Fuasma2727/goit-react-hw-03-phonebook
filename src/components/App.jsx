e80da13ed45e0380e6f6ac712513cb74d0f04885
Fuasma2727 <jmamgb127@gmail.com>
2023-10-03 22:43:17 -0500

    jkjn

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,44 +2,43 @@ import React, { useState, useEffect } from 'react';
 import ContactForm from "./ContactForm"
 import Filter from "./Filter"
 
-
-
 export const App = () => {
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const storedContacts = localStorage.getItem('contacts');
-    console.log(storedContacts);
     if (storedContacts) {
       setContacts(JSON.parse(storedContacts));
     }
   }, []);
-
-
 
   const handleAddContact = (newContact) => {
     if (contacts.some((contact) => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
       alert(`${newContact.name} is already in contacts.`);
       return;
     }
-    setContacts([...contacts, newContact]);
-    localStorage.setItem('contacts', JSON.stringify([...contacts, newContact]));
-  
+    const updatedContacts = [...contacts, newContact];
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
   };
+
   const handleDeleteContact = (id) => {
-    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+    const updatedContacts = contacts.filter((contact) => contact.id !== id);
+    setContacts(updatedContacts);
+    localStorage.setItem('contacts', JSON.stringify(updatedContacts));
   };
+
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
   };
-  const filteredContacts = contacts.filter((contact) =>
-  contact.name.toLowerCase().includes(filter.toLowerCase())
-);
 
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
-    <div 
+    <div
       style={{
         height: '100vh',
         display: 'flex',
@@ -50,17 +49,17 @@ export const App = () => {
         color: '#010101'
       }}
     >
-    <h1>Contact List</h1>
-    <ContactForm addContact={handleAddContact} contacts={contacts} />
-<Filter value={filter} onChange={handleFilterChange} />
-<ul>
-{filteredContacts.map((contact) => (
+      <h1>Contact List</h1>
+      <ContactForm addContact={handleAddContact} contacts={contacts} />
+      <Filter value={filter} onChange={handleFilterChange} />
+      <ul>
+        {filteredContacts.map((contact) => (
           <li key={contact.id}>
             {contact.name} {contact.number}
             <button onClick={() => handleDeleteContact(contact.id)}>Delete</button>
           </li>
         ))}
-</ul>
-</div>
+      </ul>
+    </div>
   );
 };
